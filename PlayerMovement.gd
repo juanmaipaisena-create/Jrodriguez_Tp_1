e464d4fcf08e8ae624747f8f85f03fac6ac54d8f
juanmaipaisena-create <juanmaipaisena@gmail.com>
2026-04-21 19:51:48 -0300

    acelereación y efecto jabon
    acelera con tiempo y tiene un efecto de patinado

--- PlayerMovement.gd
+++ PlayerMovement.gd
@@ -1,28 +1,40 @@
 extends CharacterBody2D
 
+@export var speed:= 100
+@export var aceleracion:= 50
 
-const SPEED = 150.0
+const SPEED = 100.0
 const JUMP_VELOCITY = -400.0
+const  max_jumps = 3
 
 @onready var animationPlayer = $AnimationPlayer
 @onready var sprite2D = $Sprite2D
+var  jump_count = 0
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		
+	if is_on_floor():
+		jump_count = 0
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and jump_count<max_jumps:
 		velocity.y = JUMP_VELOCITY
+		jump_count+=1
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
+	var target_velocity := direction*speed
+	
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		#efecto de jabon y velocidad 
+		velocity.x = move_toward(velocity.x, target_velocity,aceleracion * delta)
 
 	move_and_slide()
 	animacionPersonaje(direction)
